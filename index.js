5a6ee2e2ad92867d507c994cd84c67cf9e24d93d
Charptr0 <charlesofficial1@hotmail.com>
2022-05-25 21:48:54 -0400

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("canvas");
+const windowHeight = window.screen.height;
+const windowWidth = window.screen.width;
+
+canvas.width = windowWidth * 0.8;
+canvas.height = windowHeight * 0.8;
+
+console.log(windowHeight);
